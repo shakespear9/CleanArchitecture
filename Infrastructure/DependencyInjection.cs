9b954446242031a994479dd13fd6d673228cd9d9
Nuttakorn Tedthong <leendsru@hotmail.com>
2022-05-17 17:55:57 +0700

    implement efcore to project

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+// using this with IConfiguration
+using Microsoft.Extensions.Configuration;
+
+// using this with IServiceCollection
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDBContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CleanArchDatabase"), builder =>
+                 {
+                     builder.MigrationsAssembly(typeof(ApplicationDBContext).Assembly.FullName);
+                 });
+            }, ServiceLifetime.Transient);
+
+            services.AddScoped<IApplicationDBContext>(provider => provider.GetService<ApplicationDBContext>());
+
+
+
+            return services;
+
+        }
+
+
+
+    }
+}
---
+++ Infrastructure/Persistence/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Interfaces;
+using Domain.Master;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDBContext : DbContext, IApplicationDBContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppSetting>? AppSettings { get; set; }   
+
+        public Task<int> SaveChangeAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+    }
+}
